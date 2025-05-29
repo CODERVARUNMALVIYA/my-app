b9bced305b84f1287043b10dba8199e2b1bc2558
Varun Malviya <varunmalviya738@gmail.com>
2025-05-29 18:11:14 +0530

    SignUp form Validation

--- src/app/_components/restaurantSignUp.js
+++ src/app/_components/restaurantSignUp.js
@@ -11,8 +11,25 @@ const RestaurantSignUp=()=>{
   const [address, setaddress]=useState('');
   const [Mobile, setMobile]=useState('');
   const router = useRouter();
+  const [error,seterror]= useState(false);
+  const [passwordError, setpasswordError]= useState(false);
 
   const handleSignUp= async()=>{
+    if( password!== ConformPassword){
+      setpasswordError(true);
+      return false;
+    }
+    else{
+      setpasswordError(false);
+    }
+    if(!email|| !password|| !ConformPassword|| !restauranName|| !city|| !address|| !Mobile){
+      seterror(true);
+       return false;
+    }
+    else{
+      seterror(false)
+    }
+   
     console.log(email,password,ConformPassword,restauranName,city,address,Mobile);
     let response= await fetch("http://localhost:3000/api/restaurant",{
       method:"POST",
@@ -33,38 +50,65 @@ const RestaurantSignUp=()=>{
        <h3> SignUp Component</h3>
          <div >
         <div className="input-wrapper">
-            <input type="text" placeholder="Enter Email id" className="input-field"
+            <input type="email" placeholder="Enter Email id" className="input-field"
              value={email} onChange={(e)=>setemail(e.target.value)}/>
+             {
+             error && !email&& <span className="input-error"> flied are empty</span>
+            }
         </div>
       
         <div className="input-wrapper">
-            <input type="text" placeholder="Enter password" className="input-field"
+            <input type="password" placeholder="Enter password" className="input-field"
             value={password} onChange={(e)=>setpassword(e.target.value)}/>
+            {
+              passwordError && <span className="input-error">password and ConformPassword not matched</span>
+            }
+            {
+             error && !password&& <span className="input-error"> flied are empty</span>
+            }
         </div>
 
         <div className="input-wrapper">
-            <input type="text" placeholder="Conform password" className="input-field"
+            <input type="password" placeholder="Conform password" className="input-field"
             value={ConformPassword} onChange={(e)=>setConformPassword(e.target.value)}/>
+            {
+              passwordError && <span className="input-error">password and ConformPassword not matched</span>
+            }
+            {
+             error && !ConformPassword&& <span className="input-error"> flied are empty</span>
+            }
         </div>
 
           <div className="input-wrapper">
             <input type="text" placeholder="Enter Restaurant Name" className="input-field"
             value={restauranName} onChange={(e)=>setrestauranName(e.target.value)}/>
+            {
+             error && !restauranName&& <span className="input-error"> flied are empty</span>
+            }
         </div>
 
           <div className="input-wrapper">
             <input type="text" placeholder="Enter city" className="input-field"
             value={city} onChange={(e)=>setcity(e.target.value)}/>
+            {
+             error && !city&& <span className="input-error"> flied are empty</span>
+            }
         </div>
 
           <div className="input-wrapper">
             <input type="text" placeholder="Enter full Address" className="input-field"
             value={address} onChange={(e)=>setaddress(e.target.value)}/>
+            {
+             error && !address&& <span className="input-error"> flied are empty</span>
+            }
         </div>
 
           <div className="input-wrapper">
             <input type="text" placeholder="Enter Mobile Number" className="input-field"
             value={Mobile} onChange={(e)=>setMobile(e.target.value)}/>
+            {
+             error && !Mobile&& <span className="input-error"> flied are empty</span>
+            }
         </div>
         <div className="input-wrapper">
            <button className="button" onClick={handleSignUp}>SignUP</button >
